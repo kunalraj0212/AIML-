bbc33ac604eaf3b1980ecc21aa5096d0d5ce3d24
Kunal Raj <razzkunal7@gmail.com>
2026-06-30 22:27:57 +0530

    Delete day 2 AIML directory

--- day 2 AIML/02_table_of_N.py
+++
@@ -1,5 +0,0 @@
-num = int(input("enter num : "))
-
-for i in range(1,11):
-    mul = num * i
-    print(f"{num} * {i} = {mul}")
--- day 2 AIML/03_break_continue.py
+++
@@ -1,9 +0,0 @@
-
-count = 0
-
-while(count < 10):
-    count += 1
-    if(count % 2 == 0):
-        continue
-    else:
-        print(count)
